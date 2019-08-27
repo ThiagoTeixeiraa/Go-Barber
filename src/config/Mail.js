8a0064df1bb1affa1774ba55ea56847a5c4fee67
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-08-26 19:17:41 -0500

    configurado o nodemailer e envio de email após cancelamento

---
+++ src/config/Mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a20b40579956d4',
+    pass: '1c2017733b038c',
+  },
+  default: {
+    from: 'Equipe GoBarber <thiagodesousa951@gmail.com>',
+  },
+};
