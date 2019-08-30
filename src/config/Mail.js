3312c7f02a7712f1f7773da7680e5abc1e8589aa
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-08-29 23:38:09 -0300

    configuraando variaveis de ambiente

--- src/config/Mail.js
+++ src/config/Mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'a20b40579956d4',
-    pass: '1c2017733b038c',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <thiagodesousa951@gmail.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://3061657f49ec4e9e9600fd85434836e3@sentry.io/1546203',
+  dsn: process.env.SENTRY_DSN,
 };
