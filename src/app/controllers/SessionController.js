5c7b15a03b880760c2f882cfc7204b22e35537ed
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-09-28 14:01:04 -0300

    Configurando Cors

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,11 +1,21 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
+import File from '../models/File';
 import AuthConfig from '../../config/Auth';
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not exists' });
@@ -15,13 +25,14 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
       },
       token: jwt.sign({ id }, AuthConfig.secret, {
         expiresIn: AuthConfig.expiresIn,
